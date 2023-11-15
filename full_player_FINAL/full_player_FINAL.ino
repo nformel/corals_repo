779e7f11ed48d4ccfdc484350e3e7b121756c7ca
ben <bweiss@whoi.edu>
2023-11-15 13:55:51 -0500

    removing test line

--- full_player_FINAL/full_player_FINAL.ino
+++ full_player_FINAL/full_player_FINAL.ino
@@ -1,7 +1,6 @@
 //This code plays the loaded wav files on a schedule AND
 //will tell the TPL510 that it's done when Sound Off is trigered
 //final version as of 10/18/22 4:47pm
-//git is linked!
 
 //LIBRARIES
 #include <TimeLib.h>
