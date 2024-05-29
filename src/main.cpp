5395f3fbc6cf0666fa4add10621b1e9a25985034
naoki@whoi.edu <67340978+nsaoki@users.noreply.github.com>
2024-05-29 09:49:14 -1000

    changed volume to 0.8

--- src/main.cpp
+++ src/main.cpp
@@ -642,7 +642,7 @@ void setup()  {
   // WAV Player Setup
   AudioMemory(8);
   sgtl5000_1.enable();
-  sgtl5000_1.volume(0.3);
+  sgtl5000_1.volume(0.8);
 
   //Initialize the entropy funcition
   Entropy.Initialize();   
