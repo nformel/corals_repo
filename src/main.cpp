56bb4264ba00b59aca18c2f9e20717324df2290d
nformel <nathan.formel@whoi.edu>
2024-06-18 06:26:08 -1000

    needed to

--- src/main.cpp
+++ src/main.cpp
@@ -714,8 +714,8 @@ void setup()  {
 
 void loop() {
   digitalClockDisplay(); //serial print the time according to RTC
-  fault_check(); //If wavfile isn't playing, force on based on time
-  ble.print("AT+BLEUARTTX=");
-  ble.println(active_file);
+  // fault_check(); //If wavfile isn't playing, force on based on time
+  // ble.print("AT+BLEUARTTX=");
+  // ble.println(active_file);
   Alarm.delay(1000); // wait one second between clock display
 }
