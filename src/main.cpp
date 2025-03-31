2a75aada1ec88869f44bf769adc7e1630aa09274
ben <bweiss@whoi.edu>
2025-03-31 11:12:01 -0400

    trying tot figur out the rason why fault check fails second time round

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 //This code plays the loaded wav files on a schedule AND
 //will tell the TPL510 that it's done when Sound Off is trigered
-#define SOFTTWARE_VERSION "v2.2"
+#define SOFTTWARE_VERSION "test v1.2"
 
 //LIBRARIES
 #include <Arduino.h>
@@ -11,21 +11,111 @@ using namespace std;
 #define SEC_PRE_MIDNIGHT 86399
 #define MIDNIGHT_IN_SEC 86400
 #define BAUDE_RATE = 115200
+int fault_cnt = 0;
+
+//Wav Player Setup
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+#include <Entropy.h>
+#include <string>
+#include <array>
+using namespace std;
+AudioPlaySdWav           playWav1;
+AudioOutputI2S           audioOutput;
+AudioConnection          patchCord1(playWav1, 0, audioOutput, 0);
+AudioConnection          patchCord2(playWav1, 1, audioOutput, 1);
+AudioControlSGTL5000     sgtl5000_1;
+
+//STATIC DEFINITIONS
+#define SDCARD_CS_PIN    10
+#define SDCARD_MOSI_PIN  7
+#define SDCARD_SCK_PIN   14
+#define WAIT_AFTER_PLAY_MS 250
+#define SEC_PRE_MIDNIGHT 86399
+#define MIDNIGHT_IN_SEC 86400
+int done_pin = 17;
+int mos_pwr = 3;
+int mos_audio = 2;
+bool USE_SAMP = false; //set to false if not using sample number
+int sampleNumber = 0;
+#define NUM_SAMP 3 //number of samples for each FILE_BASE
 
 // Wake Time
-int startH = 16;
+int startH = 6;
 int startM = 0;
 int startS = 0;
 
 // Play Time (first alarm) [18:0:0 for real]
-int playH = 15;
+int playH = 7;
 int playM = 0;
 int playS = 0;
 
 // Sleep Time
-int stopH = 6;
+int stopH = 18;
 int stopM = 0;
 int stopS = 0;
+
+//Other
+// SD Logging file instantiation
+File myFile;
+String active_file = "no file";
+
+//settings from config file
+// Alarm times
+//ALARM TIMES
+#define ALARM_1 "7:00:00"
+#define ALARM_2 "12:51:10"
+#define ALARM_3 "12:51:20"
+#define ALARM_4 "12:51:30"
+#define ALARM_5 "12:51:40"
+#define ALARM_6 "12:51:50"
+#define ALARM_7 "12:52:00"
+#define ALARM_8 "12:52:10"
+#define ALARM_9 "12:52:20"
+#define ALARM_10 "12:52:30"
+#define ALARM_11 "12:52:40"
+#define ALARM_12 "12:52:50"
+#define ALARM_13 "12:53:00"
+#define ALARM_14 "12:53:10"
+#define ALARM_15 "12:53:20"
+#define ALARM_16 "12:53:30"
+#define ALARM_17 "12:53:40"
+#define ALARM_18 "12:53:50"
+#define ALARM_19 "12:54:00"
+#define ALARM_20 "12:54:10"
+#define ALARM_21 "12:54:20"
+#define ALARM_22 "12:54:30"
+#define ALARM_23 "12:54:40"
+#define ALARM_24 "14:54:50"
+
+// FILE BASES
+#define ALARM_1_FILE_BASE "SDTEST1"
+#define ALARM_2_FILE_BASE "SDTEST2"
+#define ALARM_3_FILE_BASE "SDTEST3"
+#define ALARM_4_FILE_BASE "SDTEST4"
+#define ALARM_5_FILE_BASE "SDTEST1"
+#define ALARM_6_FILE_BASE "SDTEST2"
+#define ALARM_7_FILE_BASE "SDTEST3"
+#define ALARM_8_FILE_BASE "SDTEST4"
+#define ALARM_9_FILE_BASE "SDTEST1"
+#define ALARM_10_FILE_BASE "SDTEST2"
+#define ALARM_11_FILE_BASE "SDTEST3"
+#define ALARM_12_FILE_BASE "SDTEST4"
+#define ALARM_13_FILE_BASE "SDTEST1"
+#define ALARM_14_FILE_BASE "SDTEST2"
+#define ALARM_15_FILE_BASE "SDTEST3"
+#define ALARM_16_FILE_BASE "SDTEST4"
+#define ALARM_17_FILE_BASE "SDTEST1"
+#define ALARM_18_FILE_BASE "SDTEST2"
+#define ALARM_19_FILE_BASE "SDTEST3"
+#define ALARM_20_FILE_BASE "SDTEST4"
+#define ALARM_21_FILE_BASE "SDTEST1"
+#define ALARM_22_FILE_BASE "SDTEST2"
+#define ALARM_23_FILE_BASE "SDTEST3"
+#define ALARM_24_FILE_BASE "SDTEST4"
 
 /////////////
 //FUNCTIONS//
@@ -116,6 +206,25 @@ char * make_time(int H, int M, int S){
   return time;
 }
 
+// playFile function from WAV file player
+void playFile(std::string filename) { //const char string[]
+  Serial.println("Playing file:");
+  Serial.println(filename.c_str());
+  playWav1.play(filename.c_str());
+  active_file = filename.c_str();
+  delay(10);
+}
+
+std::string makeFileNameString(std::string file_base, int samp, bool use_samp){
+  if(use_samp == true){
+    std::string result = file_base + std::to_string(samp) + ".wav";  
+    return result;    
+  }
+  else{
+    std::string result = file_base + ".wav";  
+    return result;       
+  }
+}
 
 //Function to determine if the present time is between two values
 bool time_between(std::string startTime, std::string stopTime) {
@@ -187,6 +296,181 @@ bool time_between(std::string startTime, std::string stopTime) {
   return rtrn;
 }
 
+void fault_check(){
+  Serial.println("run fault_check");
+  //Decide if system should be on and/or playing OR go to sleep
+  //create wake time string from integers
+  //Serial.println("load wake time");
+  char * wake_time = make_time(startH, startM, startS);
+  //Serial.println("wake_time is: ");
+  //Serial.println(wake_time);
+
+  //create play time string from integers
+  //Serial.println("load play time");
+  char * play_time = make_time(playH, playM, playS);
+  //Serial.println("play_time is: ");
+  //Serial.println(play_time);
+
+  //create sleep time string from integers
+  //Serial.println("load sleep time");
+  char * sleep_time = make_time(stopH, stopM, stopS);
+  //Serial.println("sleep_time is: ");
+  //Serial.println(sleep_time);
+
+  //Determine if sysem should be on
+  bool mode_on = time_between(wake_time, sleep_time);
+  if (mode_on == false) {
+    Serial.println("Fault: Sleep.");
+    delay(250);
+    free(wake_time);
+    free(play_time);
+    free(sleep_time);
+    digitalWrite(mos_pwr, LOW);
+    digitalWrite(mos_audio, LOW);
+    digitalWrite(done_pin, HIGH);
+    delay(1000);
+    digitalWrite(done_pin, LOW);
+  } else{
+  // Keep system on
+  Serial.println("Fault: system should be on");
+  digitalWrite(mos_pwr, HIGH);
+  digitalWrite(mos_audio, HIGH);
+
+  Serial.println("passed point 1");
+
+  free(wake_time);
+  free(play_time);
+  free(sleep_time);
+
+  Serial.println("passed point 2");
+  }
+
+  Serial.println("passed point 3");
+
+  //Determine if sysem should be playing
+  bool mode_play = time_between(play_time, sleep_time);
+
+  Serial.println("passed point 4");
+
+  if (mode_play){Serial.println("System should be playing");}
+  else {Serial.println("System should be on, but not playing yet");};
+  
+  Serial.println("passed point 5");
+
+  Serial.println("playWav1.isPlaying() == true: ");
+  Serial.println((playWav1.isPlaying() == true));
+
+  Serial.println("I made it past evaluating that statement");
+
+  if (playWav1.isPlaying() == true && mode_play == true){
+    Serial.println("System is already playing, all is well");
+  } else if (playWav1.isPlaying() == false && mode_play == true){ 
+    Serial.println("Fault: System was not playing but it should be.");
+
+    //Changed this to go for 24 hrs
+    if (time_between(ALARM_1, ALARM_2)){
+      playFile(makeFileNameString(ALARM_1_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_2, ALARM_3)){
+      playFile(makeFileNameString(ALARM_2_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_3, ALARM_4)){
+      playFile(makeFileNameString(ALARM_3_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_4, ALARM_5)){
+      playFile(makeFileNameString(ALARM_4_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_5, ALARM_6)){
+      playFile(makeFileNameString(ALARM_5_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_6, ALARM_7)){
+      playFile(makeFileNameString(ALARM_6_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_7, ALARM_8)){
+      playFile(makeFileNameString(ALARM_7_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_8, ALARM_9)){
+      playFile(makeFileNameString(ALARM_8_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_9, ALARM_10)){
+      playFile(makeFileNameString(ALARM_9_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_10, ALARM_11)){
+      playFile(makeFileNameString(ALARM_10_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_11, ALARM_12)){
+      playFile(makeFileNameString(ALARM_11_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_12, ALARM_13)){
+      playFile(makeFileNameString(ALARM_12_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_13, ALARM_14)){
+      playFile(makeFileNameString(ALARM_13_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_14, ALARM_15)){
+      playFile(makeFileNameString(ALARM_14_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_15, ALARM_16)){
+      playFile(makeFileNameString(ALARM_15_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_16, ALARM_17)){
+      playFile(makeFileNameString(ALARM_16_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_17, ALARM_18)){
+      playFile(makeFileNameString(ALARM_17_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_18, ALARM_19)){
+      playFile(makeFileNameString(ALARM_18_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_19, ALARM_20)){
+      playFile(makeFileNameString(ALARM_19_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_20, ALARM_21)){
+      playFile(makeFileNameString(ALARM_20_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_21, ALARM_22)){
+      playFile(makeFileNameString(ALARM_21_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_22, ALARM_23)){
+      playFile(makeFileNameString(ALARM_22_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }     
+    else if (time_between(ALARM_23, ALARM_24)){
+      playFile(makeFileNameString(ALARM_23_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+    else if (time_between(ALARM_24, ALARM_1)){
+      playFile(makeFileNameString(ALARM_24_FILE_BASE, sampleNumber, USE_SAMP));
+      delay(250);
+    }
+  }
+
+  else {
+    //passed point 
+  }
+  //Serial.println("I promise, it's here that I am stuck");
+}
 
 void setup()  {
   // set the Time library to use Teensy 3.0's RTC to keep time
@@ -206,6 +490,11 @@ void setup()  {
   //Serial.println("Serial print the char*p which is the return variable of foo");
   //Serial.println (p);
   //free (p);
+
+  //Digital pin configurations
+  pinMode(done_pin, OUTPUT);
+  pinMode(mos_pwr, OUTPUT);
+  pinMode(mos_audio, OUTPUT);
 
   // Software version
   Serial.println("");
@@ -251,5 +540,14 @@ void setup()  {
 void loop() {
   digitalClockDisplay(); //serial print the time according to RTC
   Serial.println();
+  Serial.print("Fault count is: ");
+  Serial.println(fault_cnt);
+
+  if (fault_cnt == 10){
+    fault_check();
+    fault_cnt = 0;
+  } else{fault_cnt += 1;}
+
+
   delay(1000); // wait one second between clock display
 }
